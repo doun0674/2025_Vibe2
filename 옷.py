9239b513b4951225e6d923d7a35ad982ff718dc0
doun0674 <go24550674@gmail.com>
2025-07-24 11:04:16 +0900

    Update 옷.py

--- 옷.py
+++ 옷.py
@@ -1,36 +1,33 @@
 import streamlit as st
 
-st.set_page_config(page_title="이모지 옷 입히기 게임", page_icon="🧸")
+st.set_page_config(page_title="이모지 옷 입히기", page_icon="🧍")
 
-st.title("🧸 겹쳐서 입히는 이모지 캐릭터 게임")
-st.markdown("얼굴 위에 옷, 모자, 소품을 겹쳐서 캐릭터를 꾸며보세요! 😍")
+st.title("🧍 겹쳐 입히는 이모지 캐릭터 게임")
+st.markdown("이모지를 겹쳐서 캐릭터를 완성해보세요! 😎")
 
-# 캐릭터 기본 선택
-face = st.selectbox("🙂 얼굴 선택", ["🙂", "😎", "😊", "🥳", "😡", "🤖", "👽"])
+# 전신 캐릭터 이모지
+body = st.selectbox("🙋 캐릭터", ["🧍", "🧍‍♂️", "🧍‍♀️", "🧑‍🎓", "🧑‍🍳", "🧑‍🚀", "🧑‍⚕️"])
 
-# 선택 UI
-hat = st.selectbox("🧢 모자 선택", ["", "🧢", "🎩", "👑", "🎓", "⛑️", "👒"])
-outfit = st.selectbox("👕 옷 선택", ["", "👕", "👗", "🧥", "🎽", "🥋", "🦺"])
-accessory = st.selectbox("🎒 소품 선택", ["", "🎒", "🕶️", "🧤", "🧣", "👟", "✨", "⭐"])
+# 옵션 선택
+hat = st.selectbox("🎩 모자", ["", "🧢", "🎩", "👑", "🎓", "⛑️", "👒"])
+outfit = st.selectbox("👕 옷", ["", "👕", "👗", "🧥", "🥋", "🦺", "🎽"])
+accessory = st.selectbox("🎒 소품", ["", "🎒", "🧤", "🧣", "👟", "🕶️", "✨", "⭐"])
 
-# 겹치기 HTML 생성
+# 겹쳐서 하나의 레이어로 표현
 html = f"""
-<div style='position: relative; width: 100px; height: 150px; margin: auto;'>
-  <div style='position: absolute; top: -10px; left: 10px; font-size: 50px;'>{hat}</div>
-  <div style='position: absolute; top: 40px; left: 10px; font-size: 50px;'>{face}</div>
-  <div style='position: absolute; top: 90px; left: 10px; font-size: 50px;'>{outfit}</div>
-  <div style='position: absolute; top: 140px; left: 10px; font-size: 40px;'>{accessory}</div>
+<div style='position: relative; width: 100px; height: 140px; margin: auto;'>
+  <div style='position: absolute; top: 0px; left: 0px; font-size: 64px; z-index: 4;'>{hat}</div>
+  <div style='position: absolute; top: 10px; left: 0px; font-size: 64px; z-index: 3;'>{body}</div>
+  <div style='position: absolute; top: 10px; left: 0px; font-size: 64px; z-index: 2;'>{outfit}</div>
+  <div style='position: absolute; top: 10px; left: 0px; font-size: 64px; z-index: 1;'>{accessory}</div>
 </div>
 """
 
-# 결과 출력
-st.markdown("### 🎨 완성된 캐릭터")
+st.markdown("### 🧩 완성된 캐릭터")
 st.markdown(html, unsafe_allow_html=True)
 
-# 리셋
-if st.button("🔄 모두 초기화"):
+if st.button("🔄 리셋"):
     st.experimental_rerun()
 
-# 푸터
 st.markdown("---")
-st.markdown("<p style='text-align:center; color:gray;'>🎀 이모지를 겹쳐서 입히는 귀여운 캐릭터 꾸미기 🎀</p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align:center; color:gray;'>✨ 캐릭터를 겹쳐서 꾸며보는 귀여운 이모지 코디 게임 ✨</p>", unsafe_allow_html=True)
