7ac025f09e1be3882bba8222b361644605afc73d
doun0674 <go24550674@gmail.com>
2025-07-24 10:51:49 +0900

    Update 두더지.py

--- 두더지.py
+++ 두더지.py
@@ -19,7 +19,7 @@ if "game_over" not in st.session_state:
 GAME_TIME = 20  # 게임 제한 시간 (초)
 GRID_SIZE = 3   # 3x3 그리드
 
-st.title("🦫 주더지 잡기 게임")
+st.title("🦫 두더지 잡기 게임")
 st.markdown("#### 제한 시간 20초 동안 주더지를 최대한 많이 잡아보세요!")
 
 # 게임 시작
